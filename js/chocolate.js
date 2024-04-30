6323512d9d1a86678121ef7eb1d8fe4184281f9c
OlgaMudrova <mudrova.om@gmail.com>
2024-04-30 12:58:58 +0300

    create index.html

---
+++ js/chocolate.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper(".swiper2", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    // Responsive breakpoints
+    breakpoints: {
+      slidesPerView: 1,
+      centeredSlides: true,
+      // when window width is <= 767px
+      768: {
+        slidesPerView: 2,
+      },
+      // when window width is <= 1199px
+      1024: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  
+    // Keyboard control
+    keyboard: {
+      enabled: true,
+    },
+  
+    // Mousewheel control
+    mousewheel: {
+      enabled: true,
+    },
+  });
